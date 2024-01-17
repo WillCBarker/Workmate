f4bf07d9d3e6ff82d93a97cbbe1438eb075348ad
Will Barker <willbarker34@gmail.com>
2024-01-17 14:07:02 -0500

    finished crud methods for event endpoint

--- backend/config/workmate_api/views/event.py
+++ backend/config/workmate_api/views/event.py
@@ -17,3 +17,44 @@ class Events(APIView):
         serializer = s.EventSerializer(objs, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        """ Create a new event """
+        
+        current_user = request.user
+
+        serializer = s.EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.validated_data['user'] = current_user
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk):
+        """ Update an event by ID """
+
+        current_user = request.user
+
+        try:
+            obj = m.Event.objects.get(pk=pk, user=current_user)
+        except m.Event.DoesNotExist:
+            return Response({"detail": "Event not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = s.EventSerializer(obj, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        """ Delete an event by ID """
+
+        current_user = request.user
+
+        try:
+            obj = m.Event.objects.get(pk=pk, user=current_user)
+        except m.Event.DoesNotExist:
+            return Response({"detail": "Event not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        obj.delete()
+        return Response({"detail": "Event deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
