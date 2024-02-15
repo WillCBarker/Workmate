82c308ecbfca7fc216d761c85d661045f4309e9c
Will Barker <willbarker34@gmail.com>
2024-02-15 16:41:23 -0500

    added home page

---
+++ workmate-frontend/src/pages/home.js
@@ -0,0 +1,13 @@
+import Navboxes from '../components/navboxes';
+import "../assets/css/home.css"
+
+function Home() {
+    return (
+        <header class="container">
+            <Navboxes />
+        </header>
+
+    )
+}
+
+export default Home;
