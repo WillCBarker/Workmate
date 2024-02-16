cda5ede04f912c26073ebe4152eab553a1c360aa
Will Barker <willbarker34@gmail.com>
2024-02-16 13:35:20 -0500

    day numbers are now positioned in the top left, added css file to alter the day element further later on

--- workmate-frontend/src/components/day.js
+++ workmate-frontend/src/components/day.js
@@ -1,12 +1,15 @@
+import "../assets/css/day.css";
+
 const Day = ({ date, isSelected, onClick, inCurrentMonth }) => {
-    const dayNumber = date.getDate();
-    const dayName = date.toLocaleDateString('en-US', { weekday: 'short' });
-    return (
-      <div className={`calendar-day ${isSelected ? 'selected' : ''} ${ !inCurrentMonth ? "not-current-month" : ""}`} onClick={onClick}>
-        <span>{dayNumber}</span>
-        <span>{dayName}</span>
-      </div>
-    );
-  };
+  const dayNumber = date.getDate();
+  const dayName = date.toLocaleDateString('en-US', { weekday: 'short' });
+  return (
+    <div className={`calendar-day ${isSelected ? 'selected' : ''} ${ !inCurrentMonth ? "not-current-month" : ""}`} onClick={onClick}>
+          <div className="day-container">
+              <p>{dayNumber}</p>
+          </div>
+    </div>
+  );
+};
 
 export default Day;
