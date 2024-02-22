52e4de2ba16d32a5f74ceafe3df682eccc8cae94
Will Barker <willbarker34@gmail.com>
2024-02-22 14:39:13 -0500

    refactoring format

--- workmate-frontend/src/components/day.js
+++ workmate-frontend/src/components/day.js
@@ -2,7 +2,7 @@ import "../assets/css/day.css";
 
 const Day = ({ date, isSelected, onClick, inCurrentMonth }) => {
   const dayNumber = date.getDate();
-  const dayName = date.toLocaleDateString('en-US', { weekday: 'short' });
+
   return (
     <div className={`calendar-day ${isSelected ? 'selected' : ''} ${ !inCurrentMonth ? "not-current-month" : ""}`} onClick={onClick}>
           <div className="day-container">
